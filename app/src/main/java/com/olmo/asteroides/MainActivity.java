41f16c30f58ae11e332f536f7e0eec94f2a04039
Olmo Tamargo <olmot.s@gmail.com>
2021-01-14 19:31:22 +0100

    segundo commit menu about

--- app/src/main/java/com/olmo/asteroides/MainActivity.java
+++ app/src/main/java/com/olmo/asteroides/MainActivity.java
@@ -2,13 +2,40 @@ package com.olmo.asteroides;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnJugar,btnConfigurar,btnPuntuaciones,btnAcercaDe,btnSalir;
+    Intent intent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnAcercaDe= (Button) findViewById(R.id.btnAcercaDe);
+        btnAcercaDe.setOnClickListener(this);
+        btnSalir= (Button) findViewById(R.id.btnSalir);
+        btnSalir.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnAcercaDe:
+                intent = new Intent(MainActivity.this, AcercaDe.class);
+                startActivity(intent);
+                break;
+
+            case R.id.btnSalir:
+                finish();
+                break;
+        }
     }
 }
