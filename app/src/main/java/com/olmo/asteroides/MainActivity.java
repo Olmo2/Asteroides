d4388a8cd756fc55f197af229d5f4d403ab76914
Olmo Tamargo <olmot.s@gmail.com>
2021-02-02 16:45:10 +0100

    añadida barra de herramientas, prefencias WIP

--- app/src/main/java/com/olmo/asteroides/MainActivity.java
+++ app/src/main/java/com/olmo/asteroides/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,12 +19,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         btnAcercaDe= (Button) findViewById(R.id.btnAcercaDe);
         btnAcercaDe.setOnClickListener(this);
         btnSalir= (Button) findViewById(R.id.btnSalir);
         btnSalir.setOnClickListener(this);
 
 
+    }
+
+    /*Para configurar la barra de tareas*/
+    @Override public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true; /** true -> el menú ya está visible */
+    }
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if (id == R.id.acercaDe) {
+            lanzarAcercaDe();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    public void lanzarAcercaDe(){
+        Intent intent=new Intent(this,AcercaDe.class);
+        startActivity(intent);
     }
 
     @Override
